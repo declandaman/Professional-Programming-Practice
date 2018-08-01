be02df4793bee0557b4fb385d6c519f750260311
declandaman <41535835+declandaman@users.noreply.github.com>
2018-08-01 17:18:34 +1000

    Add files via upload

--- src/PayFineControl.java
+++ src/PayFineControl.java
@@ -4,8 +4,8 @@ public class PayFineControl {
 	private enum CONTROL_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
 	private CONTROL_STATE state;
 	
-	private library library;
-	private member member;;
+	private Library library;
+	private Member member;;
 
 
 	public PayFineControl() {
