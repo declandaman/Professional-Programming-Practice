06f25b63890599b6961543e438237a17c2570b6d
declandaman <declan.thomson@hotmail.com>
2018-08-08 22:17:24 +1000

    Undid error

--- src/ReturnBookControl.java
+++ src/ReturnBookControl.java
@@ -1,6 +1,5 @@
 public class ReturnBookControl {
 
-<<<<<<< HEAD
     private ReturnBookUi ui;
     
     private enum ControlState { INITIALISED, READY, INSPECTING };
@@ -17,33 +16,20 @@ public class ReturnBookControl {
         
         state = ControlState.INITIALISED;
     }
-=======
-	private ReturnBookUI ui;
-	private enum CONTROL_STATE { INITIALISED, READY, INSPECTING };
-	private CONTROL_STATE state;
-	
-	private library library;
-	private loan currentLoan;
-	
 
-	public ReturnBookControl() {
-		this.library = library.INSTANCE();
-		state = CONTROL_STATE.INITIALISED;
-	}
-	
-	
-	public void setUI(ReturnBookUI ui) {
-		if (!state.equals(CONTROL_STATE.INITIALISED)) {
-			throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
-		}	
-		this.ui = ui;
-		ui.setState(ReturnBookUI.UI_STATE.READY);
-		state = CONTROL_STATE.READY;		
-	}
->>>>>>> master
+    
+    public void setUi(ReturnBookUi ui) {
+        if (!state.equals(ControlState.INITIALISED)) {
+            throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
+        }
+        
+        this.ui = ui;
+        
+        ui.setState(ReturnBookUi.UiState.READY);
+        
+        state = ControlState.READY;
+    }
 
-
-<<<<<<< HEAD
     
     public void bookScanned(int bookId) {
         if (!state.equals(ControlState.READY)) {
@@ -85,56 +71,28 @@ public class ReturnBookControl {
         
         state = ControlState.INSPECTING;
     }
-=======
-	public void bookScanned(int bookId) {
-		if (!state.equals(CONTROL_STATE.READY)) {
-			throw new RuntimeException("ReturnBookControl: cannot call bookScanned except in READY state");
-		}	
-		book currentBook = library.Book(bookId);
-		
-		if (currentBook == null) {
-			ui.display("Invalid Book Id");
-			return;
-		}
-		if (!currentBook.On_loan()) {
-			ui.display("Book has not been borrowed");
-			return;
-		}		
-		currentLoan = library.getLoanByBookId(bookId);	
-		double overDueFine = 0.0;
-		if (currentLoan.isOverDue()) {
-			overDueFine = library.calculateOverDueFine(currentLoan);
-		}
-		ui.display("Inspecting");
-		ui.display(currentBook.toString());
-		ui.display(currentLoan.toString());
-		
-		if (currentLoan.isOverDue()) {
-			ui.display(String.format("\nOverdue fine : $%.2f", overDueFine));
-		}
-		ui.setState(ReturnBookUI.UI_STATE.INSPECTING);
-		state = CONTROL_STATE.INSPECTING;		
-	}
->>>>>>> master
 
+    
+    public void scanningComplete() {
+        if (!state.equals(ControlState.READY)) {
+            throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
+        }
+        
+        ui.setState(ReturnBookUi.UiState.COMPLETED);
+    }
 
-	public void scanningComplete() {
-		if (!state.equals(CONTROL_STATE.READY)) {
-			throw new RuntimeException("ReturnBookControl: cannot call scanningComplete except in READY state");
-		}	
-		ui.setState(ReturnBookUI.UI_STATE.COMPLETED);		
-	}
-
-
-	public void dischargeLoan(boolean isDamaged) {
-		if (!state.equals(CONTROL_STATE.INSPECTING)) {
-			throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
-		}	
-		library.dischargeLoan(currentLoan, isDamaged);
-		currentLoan = null;
-		ui.setState(ReturnBookUI.UI_STATE.READY);
-		state = CONTROL_STATE.READY;				
-	}
-
-
+    
+    public void dischargeLoan(boolean isDamaged) {
+        if (!state.equals(ControlState.INSPECTING)) {
+            throw new RuntimeException("ReturnBookControl: cannot call dischargeLoan except in INSPECTING state");
+        }
+        
+        library.dischargeLoan(currentLoan, isDamaged);
+        
+        currentLoan = null;
+        
+        ui.setState(ReturnBookUi.UiState.READY);
+        
+        state = ControlState.READY;
+    }
 }
--- src/ReturnBookUi.java
+++ src/ReturnBookUi.java
@@ -1,87 +1,97 @@
 import java.util.Scanner;
 
+public class ReturnBookUi {
 
-public class ReturnBookUI {
+    public static enum UiState { INITIALISED, READY, INSPECTING, COMPLETED };
+    
+    private ReturnBookControl control;
+    
+    private Scanner input;
+    
+    private UiState state;
 
-	public static enum UI_STATE { INITIALISED, READY, INSPECTING, COMPLETED };
+    
+    public ReturnBookUi(ReturnBookControl control) {
+        this.control = control;
+        input = new Scanner(System.in);
+        
+        state = UiState.INITIALISED;
+        
+        control.setUi(this);
+    }
+    
+    
+    private String getInput(String prompt) {
+        System.out.print(prompt);
+        return input.nextLine();
+    }
 
-	private ReturnBookControl control;
-	private Scanner input;
-	private UI_STATE state;
+    
+    private void output(Object object) {
+        System.out.println(object);
+    }
 
-	
-	public ReturnBookUI(ReturnBookControl control) {
-		this.control = control;
-		input = new Scanner(System.in);
-		state = UI_STATE.INITIALISED;
-		control.setUI(this);
-	}
+    
+    public void display(Object object) {
+        output(object);
+    }
 
+    
+    public void setState(UiState state) {
+        this.state = state;
+    }
+    
+    
+    public void run() {
+        output("Return Book Use Case UI\n");
+        while (true) {
+            switch (state) {
+                
+                case INITIALISED: {
+                    break;
+                }
+                
+                case READY: {
+                    String bookIdString = getInput("Scan Book (<enter> completes): ");
+                    
+                    if (bookIdString.length() == 0) {
+                        control.scanningComplete();
+                    } 
+                    
+                    else {
+                        try {
+                            int bookId = Integer.valueOf(bookIdString).intValue();
+                            control.bookScanned(bookId);
+                        } 
+                        catch (NumberFormatException error) {
+                            output("Invalid Book Id");
+                        }
+                    }
+                    break;
+                }
+                
+                
+                case INSPECTING: {
+                    String answer = getInput("Is book damaged? (Y/N): ");
+                    boolean isDamaged = false;
+                    
+                    if (answer.toUpperCase().equals("Y")) {
+                        isDamaged = true;
+                    }
+                    
+                    control.dischargeLoan(isDamaged);
+                }
 
-	public void run() {		
-		output("Return Book Use Case UI\n");
-		
-		while (true) {
-			
-			switch (state) {
-			
-			case INITIALISED:
-				break;
-				
-			case READY:
-				String bookStr = input("Scan Book (<enter> completes): ");
-				if (bookStr.length() == 0) {
-					control.scanningComplete();
-				}
-				else {
-					try {
-						int bookId = Integer.valueOf(bookStr).intValue();
-						control.bookScanned(bookId);
-					}
-					catch (NumberFormatException e) {
-						output("Invalid bookId");
-					}					
-				}
-				break;				
-				
-			case INSPECTING:
-				String ans = input("Is book damaged? (Y/N): ");
-				boolean isDamaged = false;
-				if (ans.toUpperCase().equals("Y")) {					
-					isDamaged = true;
-				}
-				control.dischargeLoan(isDamaged);
-			
-			case COMPLETED:
-				output("Return processing complete");
-				return;
-			
-			default:
-				output("Unhandled state");
-				throw new RuntimeException("ReturnBookUI : unhandled state :" + state);			
-			}
-		}
-	}
+                case COMPLETED: {
+                    output("Return processing complete");
+                    return;
+                }
 
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return input.nextLine();
-	}	
-		
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}
-	
-			
-	public void display(Object object) {
-		output(object);
-	}
-	
-	public void setState(UI_STATE state) {
-		this.state = state;
-	}
-
-	
+                default: {
+                    output("Unhandled state");
+                    throw new RuntimeException("ReturnBookUI : unhandled state :" + state);
+                }
+            }
+        }
+    }
 }
